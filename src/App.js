bbee869e549624285bc4ff2b92a99b769a11ae44
Natiel Traore <you@example.com>
2022-12-05 15:03:10 +0200

    Added header componnent

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
 import logo from './logo.svg';
 import './App.css';
+// import 'src\components\header.js';
+import Header from './components/header';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    
+    <section class="todoapp">
+
+    <Header/>
+
+    <section class="main">
+        <input class="toggle-all"
+              type="checkbox" />
+        <ul class="todo-list">
+            <li>
+                <div class="view">
+                    <input class="toggle"
+                          type="checkbox" />
+                    <label>Todo Title</label>
+                    <button class="destroy" />
+                </div>
+                <input class="edit" />
+            </li>
+        </ul>
+    </section>
+
+    <footer class="footer">
+        <span class="todo-count"><strong>0</strong> items left</span>
+        <button class="clear-completed">Clear completed
+        </button>
+    </footer>
+
+    </section>
   );
 }
 
