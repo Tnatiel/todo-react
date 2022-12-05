cab848fae6a316a5d998f5f1c7f9987cab612d0a
Natiel Traore <you@example.com>
2022-12-05 15:06:54 +0200

    Added main component

--- src/App.js
+++ src/App.js
@@ -2,29 +2,16 @@ import logo from './logo.svg';
 import './App.css';
 // import 'src\components\header.js';
 import Header from './components/header';
+import Main from './components/main'
 
 function App() {
   return (
     
     <section class="todoapp">
 
-    <Header/>
+    <Header />
 
-    <section class="main">
-        <input class="toggle-all"
-              type="checkbox" />
-        <ul class="todo-list">
-            <li>
-                <div class="view">
-                    <input class="toggle"
-                          type="checkbox" />
-                    <label>Todo Title</label>
-                    <button class="destroy" />
-                </div>
-                <input class="edit" />
-            </li>
-        </ul>
-    </section>
+    <Main />
 
     <footer class="footer">
         <span class="todo-count"><strong>0</strong> items left</span>
--- src/components/main.js
+++ src/components/main.js
@@ -1,2 +1,21 @@
+export {}
 
-
+export default function Main() {
+    return (
+        <section class="main">
+        <input class="toggle-all"
+              type="checkbox" />
+        <ul class="todo-list">
+            <li>
+                <div class="view">
+                    <input class="toggle"
+                          type="checkbox" />
+                    <label>Todo Title</label>
+                    <button class="destroy" />
+                </div>
+                <input class="edit" />
+            </li>
+        </ul>
+    </section>
+    )
+}
